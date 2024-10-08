4db47ad8176deedbdc1a42f2d18f74ba001ebcd6
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-08 16:09:13 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,1 +1,28 @@
+function expressPain() {
+const bubble = document.getElementById("speech-bubble");
+bubble.innerHTML = "Ouch! That stings!";
+setTimeout(() => {
+bubble.innerHTML = "Hey there!";
+}, 2000);
+}
+
+function showCoordinates(event) {
+const posX = event.clientX;
+const posY = event.clientY;
+const display = document.getElementById("screen-text");
+display.innerHTML = `Coordinates: X=${posX}, Y=${posY}`;
+}
+
+function beep() {
+const screen = document.getElementById("screen-text");
+screen.innerHTML = "Beep beep beep!";
+}
+
+function reassurance() {
+const screen = document.getElementById("screen-text");
+screen.innerHTML = "No worries, I’ve got this!";
+setTimeout(() => {
+screen.innerHTML = "Beep beep beep!";
+}, 2000);
+}
 
